2af7f2c8160fbab338ab53d45abc52559c93efc0
d.chupakhin <d.chupakhin@bftcom.com>
2023-09-22 15:53:13 +0300

    added collections

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-use crate::doc::link;
+use crate::examples::matches::{Coin, RandomNumber};
+pub use crate::examples::{enums, links, matches, structures, variables, derive};
+use crate::examples::collections;
 
-mod doc;
+mod examples;
 
 fn main() {
-    link::exm_link(&String::from("Hello!"), &mut String::from("mut"));
+    enums::exm();
+    links::exm(&String::from("Hello!"), &mut String::from("mut"));
+    matches::dice_roll(8);
+    matches::luck_number(RandomNumber::Integer(777));
+    matches::luck_number(RandomNumber::Long(14));
+    matches::value_in_cents(Coin::Penny);
+    matches::value_in_cents(Coin::Nickel);
+    matches::value_in_cents(Coin::Dime);
+    matches::value_in_cents(Coin::Quarter);
+    matches::plus_one(Some(9));
+    matches::_if_let_exm(Some(7));
+    structures::exm();
+    variables::exm();
+    derive::debug::exm();
+    collections::vectors::exm();
+    collections::strings::exm();
 }
